41ed69add13d4ea59b2ae150dbfd21d776faeefa
jodelaenc <156103180+jodelaenc@users.noreply.github.com>
2024-01-30 20:16:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
-streamlit.header(🥣'Breakfast and Menu')   
-streamlit.text(🥗'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🐔'Kale,Spinach & Rocket Smoothie')
-streamlit.text(🥑🍞'Hard-Boiled Free-Range Egg')
+streamlit.header('🥣Breakfast and Menu')   
+streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔Kale,Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
