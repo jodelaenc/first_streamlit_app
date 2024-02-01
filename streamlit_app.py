234c03ea3d1bc6f592322ff0eddf184e1915fe07
jodelaenc <156103180+jodelaenc@users.noreply.github.com>
2024-02-01 20:53:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 import pandas
 import requests
-steamlit.stop()
 import snowflake.connector
+from urllib.error import URLError
+
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -34,5 +36,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
-
+steamlit.stop()
 my_cur.execute("insert into FRUIT_LOAD_LIST values('from steamlit')")
